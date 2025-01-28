6e566dd5a8716fc031efba34f3c772ec7d77e0b7
植木駿 <uekishun@22S1100441uekishun.local>
2025-01-28 18:50:45 +0900

    別ボタンのON/OFFとボタン以外クリックでOFF

--- Assets/Scripts/System/ButtonManager.cs
+++ Assets/Scripts/System/ButtonManager.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Reflection;
 //
 public class ButtonManager : MonoBehaviour
 {
     public List<Button> baseButtonList = new List<Button>();
     public List<GameObject> childButtonList = new List<GameObject>();
-
     private int lastButtonNumber;
+    public EventTrigger eventTrigger;
 
     // Start is called before the first frame update
     void Start()
@@ -26,15 +28,38 @@ public class ButtonManager : MonoBehaviour
                 baseButtonList[i].onClick.AddListener(() => OnButtonClick(j));
             }
         }
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((x) =>
+        {
+            Trigger(x);
+        });
+        eventTrigger.triggers.Add(entry);
     }
     // Update is called once per frame
     void Update()
     {
 
     }
+    ///<summary>
+    ///ボタンのON/OFFをまとめて管理する
+    ///</summary>
     void OnButtonClick(int i)
     {
-        childButtonList[i].gameObject.SetActive(!childButtonList[i].gameObject.activeSelf);
-        lastButtonNumber = i;
+        if (i != lastButtonNumber)//別のボタンを押したら前のボタンを閉じる
+        {
+            childButtonList[lastButtonNumber].gameObject.SetActive(false);
+            lastButtonNumber = i;
+        }
+        childButtonList[i].gameObject.SetActive(!childButtonList[i].gameObject.activeSelf);//押されたボタンが開いてたら閉じ、閉じてたら開く
+    }
+    void Trigger(BaseEventData baseEventData)
+    {
+        if (baseEventData.pointerId == -1)
+        {
+            childButtonList[lastButtonNumber].gameObject.SetActive(false);
+        }
+        Debug.Log("Trigger" + lastButtonNumber);
     }
 }
